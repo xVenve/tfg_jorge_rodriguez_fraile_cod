2586f6d32d051afb556e1075bad08cc06eb5b35f
Jorge Rodríguez <jrf1616@gmail.com>
2021-08-04 03:33:51 +0200

    Métodos DB implementados
    Método para insertar las medidas tomadas.
    Método para registrar dispositivos y actualizar su situación.

---
+++ Raspberry/database.py
@@ -0,0 +1,53 @@
+import mysql.connector
+
+
+def connect():
+    mydb = mysql.connector.connect(
+        host="192.168.1.40",
+        user="xvenve",
+        password="[n.A@Muz/mJpX.xf",
+        database="tfg_db",
+    )
+    return mydb
+
+
+def insert_sensor_data_DB(values):
+    mydb = connect()
+    with mydb.cursor() as mycursor:
+        sql = "INSERT INTO sensor_data (date, device, temperature, humidity, pm2_5, pm10, co, co2, intruder)" \
+              "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s); "
+        val = (
+            values["date"],
+            values["device"],
+            values["temperature"],
+            values["humidity"],
+            values["pm2_5"],
+            values["pm10"],
+            values["co"],
+            values["co2"],
+            values["intruder"],
+        )
+        mycursor.execute(sql, val)
+        mydb.commit()
+        print(val, "inserted.")
+
+
+def insert_update_device_DB(params):
+    mydb = connect()
+    with mydb.cursor() as mycursor:
+        sql = "INSERT INTO devices (id, date, status) VALUES (%s, %s, %s)"
+        val = (
+            params["device"], 
+            params["date"], 
+            params["status"],
+        )
+        try:
+            mycursor.execute(sql, val)
+            mydb.commit()
+            print(params["device"], "inserted")
+        except mysql.connector.errors.IntegrityError:
+            sql = "UPDATE devices SET date=%s, status=%s WHERE id=%s"
+            val = (params["date"], params["status"], params["device"])
+            mycursor.execute(sql, val)
+            mydb.commit()
+            print(params["device"], "updated to", params["status"])
---
+++ Server/database_commands.sql
@@ -0,0 +1,34 @@
+create database tfg_db;
+
+CREATE USER 'xvenve'@'%' IDENTIFIED VIA mysql_native_password USING '[n.A@Muz/mJpX.xf';
+
+GRANT USAGE ON *.* TO 'xvenve'@'%' REQUIRE NONE WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;
+
+GRANT ALL PRIVILEGES ON tfg_db.* TO 'xvenve'@'%' WITH GRANT OPTION;
+
+use tfg_db;
+
+CREATE TABLE sensor_data (
+    id MEDIUMINT NOT NULL AUTO_INCREMENT,
+    date DATETIME NOT NULL,
+    device VARCHAR(50) NOT NULL,
+    temperature FLOAT NOT NULL,
+    humidity FLOAT NOT NULL,
+    pm2_5 FLOAT NOT NULL,
+    pm10 FLOAT NOT NULL,
+    co FLOAT NOT NULL,
+    co2 MEDIUMINT NOT NULL,
+    intruder tinyint(1) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+SELECT * FROM sensor_data ORDER BY id DESC;
+
+CREATE TABLE devices (
+    id varchar(50) NOT NULL,
+    date varchar(50) NOT NULL,
+    status varchar(50) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+SELECT * FROM devices ORDER BY id DESC;
