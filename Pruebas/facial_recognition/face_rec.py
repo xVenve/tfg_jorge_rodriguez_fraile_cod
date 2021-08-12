7d1b7592b5c3491ea3064edfe562f7d5a462c977
Jorge Rodríguez <jrf1616@gmail.com>
2021-08-12 20:48:53 +0200

    Pruebas de reconocimiento facial

---
+++ Pruebas/facial_recognition/face_rec.py
@@ -0,0 +1,75 @@
+#! /usr/bin/python
+
+import pickle
+import time
+
+import cv2
+import face_recognition
+import imutils
+# import the necessary packages
+from imutils.video import VideoStream
+
+# Determine faces from encodings.pickle file model created from train_model.py
+encodingsP = "encodings.pickle"
+# use this xml file
+# https://github.com/opencv/opencv/blob/master/data/haarcascades/haarcascade_frontalface_default.xml
+cascade = "haarcascade_frontalface_default.xml"
+
+# load the known faces and embeddings along with OpenCV's Haar
+# cascade for face detection
+print("[INFO] loading encodings + face detector...")
+data = pickle.loads(open(encodingsP, "rb").read())
+detector = cv2.CascadeClassifier(cascade)
+
+# initialize the video stream and allow the camera sensor to warm up
+print("[INFO] starting video stream...")
+vs = VideoStream(src=0).start()
+# vs = VideoStream(usePiCamera=True).start()
+
+state = "No intruso"
+
+# loop over frames from the video file stream
+while True:
+    # grab the frame from the threaded video stream and resize it
+    # to 500px (to speedup processing)
+    frame = vs.read()
+    frame = imutils.resize(frame, width=500)
+
+    # convert the input frame from (1) BGR to grayscale (for face
+    # detection) and (2) from BGR to RGB (for face recognition)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    # detect faces in the grayscale frame
+    rects = detector.detectMultiScale(gray, scaleFactor=1.1,
+                                      minNeighbors=5, minSize=(30, 30),
+                                      flags=cv2.CASCADE_SCALE_IMAGE)
+
+    # OpenCV returns bounding box coordinates in (x, y, w, h) order
+    # but we need them in (top, right, bottom, left) order, so we
+    # need to do a bit of reordering
+    boxes = [(y, x + w, y + h, x) for (x, y, w, h) in rects]
+
+    # compute the facial embeddings for each face bounding box
+    encodings = face_recognition.face_encodings(rgb, boxes)
+    names = []
+
+    # loop over the facial embeddings
+    for encoding in encodings:
+        # attempt to match each face in the input image to our known
+        # encodings
+        matches = face_recognition.compare_faces(data["encodings"], encoding)
+
+        if True in matches:
+            state = "No intruso"
+        else:
+            state = "Intruso"
+    print(state)
+    time.sleep(1)
+# display the image to our screen
+# cv2.imshow("Facial Recognition is Running", frame)
+# key = cv2.waitKey(1) & 0xFF
+
+# do a bit of cleanup
+cv2.destroyAllWindows()
+vs.stop()
